efee10b3dc0379ea30c2ffa0db897214094cab41
Siavash Sefid Rodi <sefidrodi@googlemail.com>
2020-04-27 23:09:30 +0200

    fixed output path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
       famousfaces: './public/assets/js/famousfaces.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist/assets')
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: "/assets/js"
     },
     module: {
         rules: [
